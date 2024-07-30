b0839c1ef31ddcd949b03d83d2c72d29172d967f
Michael Ingardia <mingardia@end2endlogic.com>
2024-07-30 17:32:58 -0400

    add missing files

---
+++ app/pages/test/testPrimeFormKit.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+const data = reactive({});
+const signIn = async () => {
+  console.log("signing in ..");
+}
+</script>
+
+<template>
+  <div>test</div>
+  <FormKit type="text" label="regular input box" help="help text" placeholder="placeholder"/>
+  <Divider />
+  <div v-focustrap>
+    <FormKit type="primeInputText" label="Prime input box" help="help text" placeholder="placeholder"  validation="min:2 | max:9" />
+  </div>
+</template>
